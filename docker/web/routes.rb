407f6c2f22f7a60f567b8e7b1bce216e677c2c8c
Tomokatsu Onaga <onaga.tomokatsu.617@m.kyushu-u.ac.jp>
2025-05-30 00:45:11 +0900

    add: routes.rbを追加し、Dockerfileにコピー命令を追加

---
+++ docker/web/routes.rb
@@ -0,0 +1,122 @@
+Rails.application.routes.draw do
+
+  resources :worker_logs, only: ['index'] do
+    collection do
+      get "_contents"
+    end
+  end
+
+  post '/parameter_sets/_delete_selected', to: 'parameter_sets#_delete_selected' if OACIS_ACCESS_LEVEL >= 1
+  post '/parameter_sets/_create_runs_on_selected', to: 'parameter_sets#_create_runs_on_selected' if OACIS_ACCESS_LEVEL >= 1
+
+  resources :runs, only: ["index"] do
+    collection do
+      get "_jobs_table" # for ajax, datatables
+      post "_delete_selected" if OACIS_ACCESS_LEVEL >= 1
+    end
+  end
+
+  resources :analyses, only: [] do
+    collection do
+      get "_analyses_table" # for ajax, datatables
+    end
+  end
+
+  # Simulator-ParameterSet-Run relations
+  simulator_actions = ["index", "show"]
+  simulator_actions += ["new", "create", "edit", "update", "destroy"] if OACIS_ACCESS_LEVEL == 2
+  resources :simulators, shallow: true, only: simulator_actions do
+    collection do
+      post "_sort" # for ajax, update order of the table
+    end
+    member do
+      get "duplicate" if OACIS_ACCESS_LEVEL == 2
+      get "export_runs"  # export runs in CSV
+      get "_parameter_sets_list" # for ajax, datatables
+      get "_analyzer_list" # for ajax, datatables
+      get "_parameter_set_filters_list" # for ajax, datatables
+      get "_number_of_runs" # for number of runs table
+      get "_progress_overview" # for progress overview table
+      get "_host_parameters_field" # for ajax, get the fields for host_parameters
+      get "_default_mpi_omp" # for ajax, get the default mpi_procs and omp_threads
+      get "_cancel_create_ps"
+      post "save_filter" if OACIS_ACCESS_LEVEL >= 1
+      patch "update_filter" if OACIS_ACCESS_LEVEL >= 1
+      post "_delete_filter" if OACIS_ACCESS_LEVEL >= 1
+      get "_find_filter"
+    end
+
+    parameter_set_actions = ["show"]
+    parameter_set_actions += ["new", "create", "destroy"] if OACIS_ACCESS_LEVEL >= 1
+    resources :parameter_sets, only: parameter_set_actions do
+      collection do
+        get "_create_cli" # show CLI command for bulk creation
+      end
+      member do
+        get "duplicate" if OACIS_ACCESS_LEVEL >= 1
+        get "_runs_and_analyses" # for ajax, get
+        get "_runs_list" # for ajax, datatables
+        get "_analyses_list" # for ajax, datatables
+        get "_similar_parameter_sets_list" # for ajax, datatables
+        get "_files_list" # for ajax, datatables
+        get "download_result_files" # for download result files as zip
+        get "_line_plot" # for line plot
+        get "_scatter_plot" # for scatter plot
+        get "_figure_viewer" # for figure viewer
+        get "_neighbor"
+      end
+
+      run_actions = ["show"]
+      run_actions += ["create"] if OACIS_ACCESS_LEVEL >= 1
+      analysis_actions = ["show"]
+      analysis_actions += ["create", "destroy"] if OACIS_ACCESS_LEVEL >= 1
+      resources :runs, only: run_actions do
+        member do
+          get "_analyses_list" # for ajax, datatables
+        end
+        resources :analyses, only: analysis_actions do
+          member do
+            get "_result" # for ajax
+          end
+        end
+      end
+      resources :analyses, only: analysis_actions
+    end
+
+    analyzer_actions = ["show"]
+    analyzer_actions += ["new", "create", "edit", "update", "destroy"] if OACIS_ACCESS_LEVEL == 2
+    resources :analyzers, only: analyzer_actions do
+      member do
+        get "_parameters_form" # for ajax
+        get "_inner_show" # for ajax, get
+        get "_host_parameters_field" # for ajax, get the fields for host_parameters
+        get "_default_mpi_omp" # for ajax, get the default mpi_procs and omp_threads
+      end
+    end
+  end
+
+  host_actions = ["index", "show"]
+  host_actions += ["new", "create", "edit", "update", "destroy"] if OACIS_ACCESS_LEVEL == 2
+  resources :hosts, only: host_actions do
+    member do
+      get "_check_scheduler_status" if OACIS_ACCESS_LEVEL >= 1
+      get "_toggle_status" if OACIS_ACCESS_LEVEL == 2
+    end
+    collection do
+      post "_sort" # for ajax, update order of the table
+    end
+  end
+
+  host_group_actions = ["show"]
+  host_group_actions += ["new", "create", "edit", "update", "destroy"] if OACIS_ACCESS_LEVEL == 2
+  resources :host_groups, only: host_group_actions do
+  end
+
+  resources :notification_events, only: :index
+  resource :oacis_setting, only: %i[edit update] if OACIS_ACCESS_LEVEL == 2
+
+  root :to => "simulators#index"
+
+  require 'sidekiq/web'
+  mount Sidekiq::Web, at: "/sidekiq"
+end
