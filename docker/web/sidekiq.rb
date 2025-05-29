da31092b42427e0a94025a2ce51ff67e3b808229
Tomokatsu Onaga <onaga.tomokatsu.617@m.kyushu-u.ac.jp>
2025-05-30 02:56:30 +0900

    add: DockerfileとGemfileを更新し、Sidekiqのcronジョブ設定を追加

--- docker/web/sidekiq.rb
+++ docker/web/sidekiq.rb
@@ -5,3 +5,7 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://redis:6379/0' }
 end
+
+# sidekiq_cronの設定
+schedule_file = 'config/schedule.yml'
+Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file) && Sidekiq.server?
