441bf25264dc96bfb1e9b73fc4342343acaac9c9
Tomokatsu Onaga <onaga.tomokatsu.617@m.kyushu-u.ac.jp>
2025-06-04 21:45:18 +0900

    add: SidekiqのRedis設定を修正し、ミドルウェアを追加

--- docker/web/config/initializers/sidekiq.rb
+++ docker/web/config/initializers/sidekiq.rb
@@ -1,7 +1,19 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis:6379/0' }
+  config.redis = { url: 'redis://redis:6379/2' }
+
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
+
+  config.server_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Server
+  end
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis:6379/0' }
+  config.redis = { url: 'redis://redis:6379/2' }
+
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
 end
